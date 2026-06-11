7c60bd9a0343c3b661f1c68bf8a51873d3cdc45d
maidasilvacoelhinha-hue <maidasilvacoelhinha@gmail.com>
2026-06-10 21:41:37 -0300

    Ajuste

--- config/urls.py
+++ config/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("", include("meuapp.urls")),
+    path("catalogo/", include("catalogo.urls")),
     path('admin/', admin.site.urls),
 ]
